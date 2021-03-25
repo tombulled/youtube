21f9f60f7f0b08f4f4a4b849bc047e628138d037
Tom Bulled <26026015+tombulled@users.noreply.github.com>
2021-03-25 16:12:11 +0000

    Move @client.wraps to @decorators.proxy

--- youtube/client.py
+++ youtube/client.py
@@ -13,18 +13,6 @@ from . import operations
 from . import parsers
 from . import decorators
 
-def wraps(wrapped):
-    def decorator(func):
-        return_annotation = getattr(func, '__annotations__', {}).get('return')
-
-        wrapper = functools.wraps(wrapped)(func)
-
-        getattr(wrapper, '__annotations__', {})['return'] = return_annotation
-
-        return wrapper
-
-    return decorator
-
 class YouTube(innertube.ClientGroup):
     def __init__(self, locale: babel.Locale = None):
         super().__init__ \
@@ -39,7 +27,7 @@ class YouTube(innertube.ClientGroup):
             locale = locale,
         )
 
-    @wraps(innertube.operations.complete_search)
+    @decorators.proxy(innertube.operations.complete_search)
     def complete_search(self, *args, **kwargs) -> typing.List[str]:
         return super().complete_search \
         (
@@ -50,26 +38,26 @@ class YouTube(innertube.ClientGroup):
         )
 
     @decorators.parse(parsers.parse_watch)
-    @wraps(operations.watch)
+    @decorators.proxy(operations.watch)
     def watch(self, *args, **kwargs) -> models.WatchResponse:
         return operations.watch(*args, **kwargs)
 
     @decorators.parse(parsers.parse_video_info)
-    @wraps(operations.video_info)
+    @decorators.proxy(operations.video_info)
     def video_info(self, *args, **kwargs) -> models.PlayerResponse:
         return operations.video_info(*args, **kwargs)
 
     @decorators.parse(parsers.parse_guide)
-    @wraps(innertube.clients.Client.guide)
+    @decorators.proxy(innertube.clients.Client.guide)
     def guide(self, *args, **kwargs) -> typing.List[models.Pivot]:
         return self(innertube.enums.DeviceType.ANDROID).guide(*args, **kwargs)
 
     @decorators.parse(parsers.parse_player)
-    @wraps(innertube.clients.Client.player)
+    @decorators.proxy(innertube.clients.Client.player)
     def player(self, *args, **kwargs) -> models.PlayerResponse:
         return self(innertube.enums.DeviceType.ANDROID).player(*args, **kwargs)
 
     @decorators.parse(parsers.parse_next)
-    @wraps(innertube.clients.Client.next)
+    @decorators.proxy(innertube.clients.Client.next)
     def next(self, *args, **kwargs) -> models.WatchNextResponse:
         return self(innertube.enums.DeviceType.WEB).next(*args, **kwargs)
